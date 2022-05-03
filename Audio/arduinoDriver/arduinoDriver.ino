2ff1f48cdc39e986a595e8d10127163d17a729c3
Seth Braddock <sethbrad@iastate.edu>
2022-05-02 19:00:18 -0500

    arduino sketch tweaks

--- Audio/arduinoDriver/arduinoDriver.ino
+++ Audio/arduinoDriver/arduinoDriver.ino
@@ -1,53 +1,80 @@
 #include <Arduino.h>
 
-#define notCS 13
+#define NCS 13
 #define SCK 12
 #define SDI 11
+#define OUT A0
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(notCS,OUTPUT); // CS' load enable
-  pinMode(SCK,OUTPUT); // SCK clock
-  pinMode(SDI,OUTPUT); // SDI data
+// 40kHz sample rate
+// 25us delay time
+short sinTable[360];
 
-  digitalWrite(notCS,HIGH);
-  delay(50);
+void generateTable() {
+  for(int i=0; i<360; i++) {
+    sinTable[i] = (short) ((sin(i * PI / 180.0) * 2048.0) + 2048);
+  }
 }
 
-void loadBit(uint8_t bit){
+void setup() {
+  Serial.begin(9600);
+  
+  // put your setup code here, to run once:
+  pinMode(NCS,OUTPUT); // CS' load enable
+  pinMode(SCK,OUTPUT); // SCK clock
+  pinMode(SDI,OUTPUT); // SDI data
+  pinMode(OUT,INPUT);
+
+  digitalWrite(NCS,HIGH);
+  delay(1);
+}
+
+void loadBit(char bit){
   digitalWrite(SCK,LOW);
   digitalWrite(SDI,bit);
   digitalWrite(SCK,HIGH);
 }
 
-// as is this is just writing a random DC value to the DAC
+void loadData(short data) {
+  short mask = 0x0800;
+
+  for(int i=0; i<12; i++) {
+    if(mask & data) {
+      loadBit(HIGH);
+    }else{
+      loadBit(LOW);
+    }
+
+    mask = mask >> 1;
+  }
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
+  generateTable();
 
-  // begin programming
-  digitalWrite(notCS,LOW);
+  // have to convert from range 0-100 to 0-Vref (in step of 2^12)
+  for(int x=0; x<360; x++) {
 
-  // config bits
-  loadBit(LOW);
-  loadBit(LOW);
-  loadBit(HIGH);
-  loadBit(HIGH);
+    // begin programming
+    digitalWrite(NCS,LOW);
   
-  // data bits --> random value
-  loadBit(LOW);
-  loadBit(HIGH);
-  loadBit(LOW);
-  loadBit(HIGH);
-  loadBit(LOW);
-  loadBit(HIGH);
-  loadBit(HIGH);
-  loadBit(LOW);
-  loadBit(HIGH);
-  loadBit(LOW);
-  loadBit(LOW);
-  loadBit(HIGH);  
+    // config bits
+    loadBit(LOW);
+    loadBit(LOW);
+    loadBit(HIGH);
+    loadBit(HIGH);
+    
+    // data bits --> random value
+    loadData(sinTable[x]);  
+  
+    digitalWrite(NCS,HIGH);
+  
+    // view on serial plotter
+    int val = analogRead(OUT);
+    Serial.println(val, 1);
+    delayMicroseconds(10);
 
-  digitalWrite(notCS,HIGH);
-  delay(50);
-//  exit(0);
+//    Serial.println(sinTable[x]);
+  }
+  return;
 }
